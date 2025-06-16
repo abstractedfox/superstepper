3bf39ba4655832392831044aa5bc1b9c64bd7429
Kira K <chriswhoprograms@gmail.com>
2025-06-16 01:01:31 -0400

    Refine usage of chart caches in the APISession class

--- static/chart.js
+++ static/chart.js
@@ -1,15 +1,29 @@
+import { setCurrentSession } from "./main.js";
+
 let _sessions = {};
+
+export function getSession(sessionID){
+    return _sessions[sessionID];
+}
 
 export class APISession{
     constructor(port, chartFilename, rawData = null){
         this.port = port;
         this.ID = null;
-        
-        let initResponse = this.request({functionName: "init", filename: chartFilename, raw_chart: rawData});
-        initResponse.then((response) => {
+
+        //useful fields for holding data retrieved from this session. _cache suffix indicates that these are not guaranteed to hold the current state of the chart 
+        this.notes_cache = null;
+        this.measures_cache = null;
+        this.bpms_cache = null;
+
+        this.initResponse = this.request({functionName: "init", filename: chartFilename, raw_chart: rawData});
+        this.initResponse.then((response) => {
             this.ID = response["head"]["id"];
         });
+    }
 
+    async isInitialized_awaitable(){
+        return this.initResponse;
     }
 
     async request({functionName = "", changes = [], data = {}, filename = null, raw_chart = null}){
@@ -17,6 +31,8 @@ export class APISession{
         if (functionName != "init"){
             newRequest["head"]["id"] = this.ID;
         }
+
+        await this.isInitialized_awaitable();
 
         switch(functionName){
             case "init":
@@ -50,7 +66,7 @@ export class APISession{
             default:
                 throw new Error("Invalid function " + functionName);
         }
-        
+
         let response = await fetch(`http://127.0.0.1:${this.port}/api`, {
             body: JSON.stringify(newRequest),
             method: "POST",
@@ -61,13 +77,32 @@ export class APISession{
 
         return response.json();
     }
-}   
+    
+    async updateCaches({steps = false, bpms = false, measures = false}){
+        if (steps){
+            this.notes_cache = (await this.request({functionName: "get_steps"}))["data"]["steps"]; 
+        }
+        if (bpms){
+            this.bpms_cache = (await this.request({functionName: "get_bpms"}))["data"]["bpms"];
+        }
+        if (measures){
+            this.measures_cache = (await this.request({functionName: "get_measures"}))["data"]["measures"]; 
+        }
+    }
+}
 
-export function uploadChart(){
-    console.log(document.getElementById("chart_upload"));
-    chartRawFile = document.getElementById("chart_upload").files[0];
-    console.log(chartRawFile.text());
-    processChart();
+export async function uploadChart(){
+    let chartRawFile = document.getElementById("chart_upload").files[0];
+    
+    chartRawFile.text().then(async (file) => {
+        let session = new APISession(document.getElementById("port").value, document.getElementById("chart_upload").value, file);
+        await session.updateCaches({steps: true, bpms: true, measures: true}); 
+        console.log(session.notes_cache);
+        console.log(session.bpms_cache);
+        console.log(session.measures_cache);
+        _sessions[session.ID] = session;
+        setCurrentSession(session.ID);
+    });
 }
 
 //we should be able to either nuke or otherwise rewrite this
@@ -75,7 +110,7 @@ export async function processChart(){
     if (chartRawFile != null){
         const parser = new DOMParser();
         const parsed = parser.parseFromString(await chartRawFile.text(), "application/xml");
-        
+
         //boilerplate, to see how this works
         console.log("parsed:", parsed);
         console.log(parsed.children[0].children[0]);
