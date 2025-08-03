3adc42b0d6c76d551516a2c70021aa523892149f
Kira Kaviani <chriswhoprograms@gmail.com>
2025-08-03 15:08:50 -0400

    establish ChartSession type, add set_from_xml

--- static/chart.js
+++ static/chart.js
@@ -7,6 +7,110 @@ export function getSession(sessionID){
     return _sessions[sessionID];
 }
 
+//Statefully hold a chart that is being worked on
+//This uses api_port to point to a running api instance that can be used to process to and from xml as needed
+export class ChartSession{
+    constructor(api_port, rawData = null){
+        this.port = port;
+        this.ID = null;
+
+        //useful fields for holding data retrieved from this session. _cache suffix indicates that these are not guaranteed to hold the current state of the chart 
+        this.notes_cache = null;
+        this.measures_cache = null;
+        this.bpms_cache = null;
+        this.filename = chartFilename;
+    
+        this.initResponse = this.request({functionName: "init", filename: chartFilename, raw_chart: rawData});
+        this.initResponse.then((response) => {
+            this.ID = response["head"]["id"];
+        });
+    }
+
+    async set_from_xml(rawData){
+        await result = this.request({"function": "parse_chart"});
+        this.notes_cache = result["data"]["steps"];
+        this.measures_cache = result["data"]["measures"];
+        this.bpms_cache = result["data"]["bpms"];
+    }
+
+    async isInitialized_awaitable(){
+        return this.initResponse;
+    }
+
+
+    async request({functionName = "", changes = [], data = {}, filename = null, raw_chart = null}){
+        let extraErrorChecks = false; 
+
+        let newRequest = {"head": {"function": functionName}, "data": data};
+        if (functionName != "init"){
+            newRequest["head"]["id"] = this.ID;
+        }
+
+        await this.isInitialized_awaitable();
+
+        switch(functionName){
+            case "init":
+                newRequest["data"]["filename"] = filename;
+                if (raw_chart != null){
+                    newRequest["data"]["raw_chart"] = raw_chart;
+                }
+
+            case "save":
+                break
+
+            case "close_sesion":
+                break
+
+            case "update_chart":
+                if (extraErrorChecks){
+                    console.assert(changes.length != 0, "changes.length != 0");
+                    for (let x in changes){
+                        console.assert(changes[x] != undefined && changes[x] != null && changes[x] != 0, "found invalid value in changes," + changes[x]);
+                    }
+                }
+                newRequest["data"]["changes"] = changes;
+                break
+
+            case "get_steps":
+                break
+
+            case "get_bpms":
+                break
+
+            case "get_measures":
+                break
+
+            case "introspect_has_session":
+                break
+
+            default:
+                throw new Error("Invalid function " + functionName);
+        }
+
+        let response = await fetch(`http://127.0.0.1:${this.port}/api`, {
+            body: JSON.stringify(newRequest),
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+
+        return response.json();
+    }
+
+
+    async updateCaches({steps = false, bpms = false, measures = false}){
+        if (steps){
+            this.notes_cache = (await this.request({functionName: "get_steps"}))["data"]["steps"]; 
+        }
+        if (bpms){
+            this.bpms_cache = (await this.request({functionName: "get_bpms"}))["data"]["bpms"];
+        }
+        if (measures){
+            this.measures_cache = (await this.request({functionName: "get_measures"}))["data"]["measures"]; 
+        }
+    }
+}
 
 export class APISession{
     constructor(port, chartFilename, rawData = null){
